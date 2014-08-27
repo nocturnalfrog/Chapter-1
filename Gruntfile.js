d333ce3a586e7a67f501231057d2a8414fd62b6d
Tjerk Ameel <tjerk.ameel@gmail.com>
2014-08-27 21:08:38 +0200

    Solution

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,72 @@ module.exports = function (grunt) {
 
     var config = {};
 
+    config['clean'] = {
+        build: {
+            dot: true,
+            src: [
+                'dist/*',
+                '!dist/.git*'
+            ]
+        }
+    };
+
+    config['htmlmin'] = {
+        build: {
+            options: {
+                collapseBooleanAttributes: true,
+                removeAttributeQuotes: true,
+                removeRedundantAttributes: true,
+                removeEmptyAttributes: true
+            },
+            files: [{
+                expand: true,
+                cwd: 'src',
+                src: '{,*/}*.html',
+                dest: 'dist'
+            }]
+        }
+    };
+
+    config['useminPrepare'] = {
+        options: {
+            dest: 'dist'
+        },
+        html: 'src/index.html'
+    };
+
+    config['usemin'] = {
+        options: {
+            dirs: ['dist']
+        },
+        html: ['dist/{,*/}*.html']
+    };
+
+        config['uglify'] = {
+        options: {
+            mangle: false
+        }
+    };
+
+    config['rev'] = {
+        files: {
+            src: [
+                'dist/scripts/{,*/}/*.js'
+            ]
+        }
+    };
+
     grunt.initConfig(config);
 
-    var tasks = [];
+    var tasks = [
+        'clean',
+        'useminPrepare',
+        'htmlmin',
+        'concat',
+        'uglify',
+        'rev',
+        'usemin'
+    ];
 
     grunt.registerTask('build', tasks);
 };
